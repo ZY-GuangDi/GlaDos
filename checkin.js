75759a547dc9e4040997c7b18952a9a25c5f2955
xlzy <1017866168@qq.com>
2021-05-07 11:04:32 +0800

    feat:  支持多个账户签到

--- checkin.js
+++ checkin.js
@@ -1,7 +1,16 @@
 const axios = require("axios");
 
 const SCKEY = process.env.SCKEY;
-axios.defaults.headers.common.cookie = process.env.COOKIE;
+const COOKIES = process.env.COOKIES;
+// const COOKIES = `_ga=GA1.2.1274713829.1614584394; koa:sess=eyJ1c2VySWQiOjcyMzIzLCJfZXhwaXJlIjoxNjQwNTA0ODQ0ODEzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=kJDQTUOnrLjDCRtt5fR9RAIoU_Q; __cfduid=d2ca31caf1a1137ee1051c1a66efd912b1620268204; _gid=GA1.2.949445040.1620355428--分隔符--_ga=GA1.2.131316036.1620356397; _gat_gtag_UA_104464600_2=1; _gid=GA1.2.711212291.1620356397; __cfduid=d7bfedc3e59a37fea9f351e4a45c09fc41620356393; koa:sess=eyJ1c2VySWQiOjcyMzIyLCJfZXhwaXJlIjoxNjQxOTk0NzgxMTkwLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=kYJ8Km7vkjkkJgZlFNjk1gpXmeM`;
+const cookiesList = COOKIES.split('--分隔符--')
+
+cookiesList.forEach(v=>{
+  setTimeout(()=>{
+    axios.defaults.headers.common.cookie = v;
+    GLaDOSCheckIn();
+  }, 3000)
+})
 
 const checkIn = async () => {
     return axios({
@@ -38,5 +47,3 @@ const GLaDOSCheckIn = async () => {
         server(checkInMessage, leftDays);
     }
 }
-
-GLaDOSCheckIn();
